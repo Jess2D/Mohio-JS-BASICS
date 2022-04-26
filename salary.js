f8231cc1860063f68f572ebd85d0aa89f4aa10e0
Jessica Silveira <jessicap_silveira@hotmail.com>
2022-04-27 11:49:25 +1200

    Request a number and display a row of that many stars (*)

--- salary.js
+++ salary.js
@@ -3,7 +3,7 @@
 for (i = 0; i <= 9; i++) {
   console.log(i);
 }*/
-
+/*
 //Display a row of 20 stars (*)
 let text = "*";
 let i = 0;
@@ -12,4 +12,15 @@ do {
   i++;
   row += text;
 } while (i < 20);
+console.log(row);*/
+
+//Request a number and display a row of that many stars (*)
+var number = prompt("How many stars would you like?");
+var text = "*";
+var i = 0;
+var row = "";
+do {
+  i++;
+  row += text;
+} while (i < number);
 console.log(row);

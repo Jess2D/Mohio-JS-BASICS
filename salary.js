1a92878c9f12652012d35a4c219ca73e3cf407f8
Jessica Silveira <jessicap_silveira@hotmail.com>
2022-04-27 11:58:30 +1200

    Find out the sum from 1 + 1/2 + 1/3 + 1/4 +..+ 1/100

--- salary.js
+++ salary.js
@@ -28,8 +28,17 @@ console.log(row);
 */
 
 //Find out the sum from 1 to 20
+/*
 let sum = 0;
 for (i = 1; i <= 20; i++) {
   sum += i;
+}
+console.log(sum);
+*/
+
+//Find out the sum from 1 + 1/2 + 1/3 + 1/4 +..+ 1/100
+let sum = 0;
+for (i = 1; i <= 100; i++) {
+  sum += 1 / i;
 }
 console.log(sum);
